1c037a9a67c8d98e79af5184711b2dd630190ba4
shenyiling <jzx19951028yyjy@163.com>
2019-04-20 23:32:39 +0800

    fix: mock data for demo07 && fix style of it.

---
+++ mock/demo07.js
@@ -0,0 +1,25 @@
+function mockData() {
+    return Mock.mock({
+        '2018-08|31': [
+            {
+                'date|+1': 1,
+                'value|10-30': 1,
+                'verifyRequired|1': true
+            }
+        ],
+        '2018-09|30': [
+            {
+                'date|+1': 1,
+                'value|10-30': 1,
+                'verifyRequired|1': true
+            }
+        ],
+        '2018-10|31': [
+            {
+                'date|+1': 1,
+                'value|10-30': 1,
+                'verifyRequired|1': true
+            }
+        ],
+    })
+}
