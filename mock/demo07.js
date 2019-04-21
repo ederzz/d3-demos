fcade445d69a64462179ab5d2ba1977ecaa98967
shenyiling <jzx19951028yyjy@163.com>
2019-04-21 14:57:24 +0800

    feat: add re-fresh func of demo07.

--- mock/demo07.js
+++ mock/demo07.js
@@ -1,20 +1,20 @@
 function mockData() {
     return Mock.mock({
-        '2018-08|31': [
+        'prev|31': [
             {
                 'date|+1': 1,
                 'value|10-30': 1,
                 'verifyRequired|1': true
             }
         ],
-        '2018-09|30': [
+        'current|30': [
             {
                 'date|+1': 1,
                 'value|10-30': 1,
                 'verifyRequired|1': true
             }
         ],
-        '2018-10|31': [
+        'next|31': [
             {
                 'date|+1': 1,
                 'value|10-30': 1,
